f2eb95885420ab160df0a795cdda91c9c216160c
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-05-06 22:49:30 +0900

    [Start] Task10d

---
+++ app/models/book.rb
@@ -0,0 +1,33 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  validates :rate, presence: true
+  validates :rate, numericality: {
+    only_integer: true,
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1,
+  }
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+end
